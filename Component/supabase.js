23df1a05b13a2163755a2e9096a060db0020616d
aditya <aditya_chandra@srmap.edu.in>
2024-12-30 22:34:48 +0530

    third commit

--- Component/supabase.js
+++ Component/supabase.js
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const supabaseUrl = ''
-const supabaseAnonKey = ''
+const supabaseUrl = 'https://rkcmmuysdzgovnyqznlb.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrY21tdXlzZHpnb3ZueXF6bmxiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUyMzgyMTYsImV4cCI6MjA1MDgxNDIxNn0.7TCzOt6jlRTlme7yIyrKwWgH_UKOWkn9F7VjJMQb_Hw'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
